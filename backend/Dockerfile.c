c5daa8b55506d27abb7272fc2a3dc72481666414
Siddhivinayak06 <siddhivinayaksawant04@gmail.com>
2025-09-10 20:18:17 +0530

    Update Dockerfile.c

--- backend/Dockerfile.c
+++ backend/Dockerfile.c
@@ -3,3 +3,6 @@ FROM gcc:12.2
 RUN apt-get update && apt-get install -y coreutils && rm -rf /var/lib/apt/lists/*
 
 WORKDIR /app
+# Create a non-root user for running code
+RUN useradd -m runner
+USER runner
