5e06e1ac9d20b62820ed094fbb9d0e8cc8abc774
Siddhivinayak06 <siddhivinayaksawant04@gmail.com>
2025-10-11 18:07:39 +0530

    removed

--- backend1/src/server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, async () => {
-  
-  console.log(`Backend server running on http://localhost:${PORT}`);
-});
